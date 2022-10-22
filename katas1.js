dbbdb2538cdca8999932c761c5551f0e84613c4d
Marcelo-Cunha560 <113524572+Marcelo-Cunha560@users.noreply.github.com>
2022-10-22 11:43:08 -0400

    att

--- katas1.js
+++ katas1.js
@@ -1,9 +1,75 @@
-function cubicSquare(){
+// Questão 1
+// function cubicSquare(){
 
-    for(let cont = 1; cont <= 10; cont++){
-        potencia = cont ** 3
-        console.log(potencia)
+//     for(let cont = 1; cont <= 10; cont++){
+//         potencia = cont ** 3
+//         console.log(potencia)
+//     }
+// }
+
+// cubicSquare()
+
+//Questão 02
+// function divisibleFor(numero, divisor){
+//     numeroDivisivel = 0
+//     for(let i = 1; i <= numero; i++){
+//         if(i % divisor == 0){
+//             console.log(i)
+//         }
+//     }
+    
+// }
+
+
+// divisibleFor(100, 10)
+
+//Questão 3
+// function stringElement (string, Number){
+//     for(let i = 0; i <= Number; i++){
+//         if(Number > 0){
+//             novaString = ""
+//             novaString += string[Number]
+//         }
+//     }
+//     console.log(novaString.toUpperCase())
+// }
+
+// stringElement("algorítmos", 7)
+
+//Questaõ 4
+
+function stringSlicer(string, Number){
+    for(let i = 0; i < Number; i++){
+        novaString = " "
+        novaString += string[i]
+        console.log(novaString)
+        
     }
+    console.log(novaString)
+   
 }
+stringSlicer("ALGORÌTMOS", 4)
 
-cubicSquare()
+
+
+
+
+
+
+
+
+
+
+
+// function removeEspaço(string){
+
+//     for(let i = 0; i >= 0; i++){
+//         let palavra = " "
+//         if(string[i] !== " "){
+//           palavra += string[i]
+//         }
+//     }
+    
+// }
+
+// removeEspaço("Marcelo Cunha")
